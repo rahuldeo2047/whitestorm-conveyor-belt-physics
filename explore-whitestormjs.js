21a43966d48b39b81016c8a7a706180023a7f74a
Rahul <rahuldeo2047@gmail.com>
2017-05-06 17:40:38 +0530

    Update explore-whitestormjs.js
    Working example for constant speed.

--- explore-whitestormjs.js
+++ explore-whitestormjs.js
@@ -1,160 +1,132 @@
-// Try it on http://playground.whsjs.io
+//Try on http://codepen.io/Rick2047/pen/NjwqmO
 
-const world = new WHS.World({
-  stats: "fps", // fps, ms, mb or false if not need.
-  autoresize: {
-    delay: 1
-  },
-
-  gravity: { // Physic gravity.
-      x: 0,
-      y: -100,
+const app = new WHS.App([
+  new WHS.app.ElementModule(),
+  new WHS.app.SceneModule(),
+  new WHS.app.CameraModule({
+    position: {
+      x: 400,
+      y: 0,
       z: 0
-  },
-
-  camera: {
-    position: [100, 100, 100]
-  },
-
-  rendering: {
-    background: {
-      color: 0x162129
-    },
+    }
+  }),
+  new WHS.app.RenderingModule({
+    bgColor: 0x162129,
 
     renderer: {
-      antialias: true
+      antialias: true,
+      shadowmap: {
+        type: THREE.PCFSoftShadowMap
+      }
     }
-  },
+  }, {shadow: true}),
+  new PHYSICS.WorldModule({
+    ammo: 'https://cdn.rawgit.com/WhitestormJS/physics-module-ammonext/75634e80/vendor/ammo.js'
+  }),
+  new WHS.controls.OrbitModule()
+]);
 
-  container: document.body
-});
-
+// Sphere
 const sphere = new WHS.Sphere({ // Create sphere comonent.
   geometry: {
-    radius: 3,
+    radius: 5,
     widthSegments: 32,
     heightSegments: 32
   },
+  
+  modules: [
+    new PHYSICS.SphereModule({
+      mass: 10
+    })
+  ],
 
-  mass: 10, // Mass of physics object.
+  material: new THREE.MeshPhongMaterial({
+    color: 0xF2F2F2
+  }),
 
-  material: {
-    color: 0xF2F2F2,
-    kind: 'lambert'
-  },
-
-  position: {
-    x: 0,
-    y: 100,
-    z: 0
-  }
+  position: new THREE.Vector3(0, 15, 0)
 });
 
-const plane = new WHS.Plane({
+//sphere.addTo(app);
+
+// Plane
+new WHS.Plane({
   geometry: {
     width: 100,
     height: 100
   },
+  
+  modules: [
+    new PHYSICS.PlaneModule({
+      mass: 0
+    })
+  ],
 
-  mass: 0,
-
-  material: {
-    color: 0x447F8B,
-    kind: 'phong'
-  },
+  material: new THREE.MeshPhongMaterial({color: 0x447F8B}),
 
   rotation: {
-    x: - Math.PI / 2
+    x: -Math.PI / 2
   }
-});
-
-new WHS.PointLight({
-  light: {
-    intensity: 0.5
-  },
-
-  shadowmap: {
-    fov: 90
-  },
-
-  position: {
-    z: 10,
-    y: 10
-  }
-}).addTo(world);
-
-new WHS.AmbientLight({
-  light: {
-    intensity: 0.5
-  }
-}).addTo(world);
+}).addTo(app);
 
 const box = new WHS.Box({
-  geometry: {
-    width: 100,
-    height: 2,
-    depth: 300
-  },
-  
-  mass: 10, // Mass of physics object.
-
-  material: {
-    color: 0xF2F2F2,
-    kind: 'lambert'
-  },
-
-  position: {
-    x: 0,
-    y: 1,
-    z: 0
-  }
-   
-});
-
-// https://github.com/WhitestormJS/physics-module-ammonext/blob/master/src/modules/WorldModule.js#L487
-fnCollide = function(otherObject, v, r, contactNormal)
-{
-    // None work
- window.alert("Contact");
-  //otherObject.setLinearVelocity(new Vector3(0,200,200));  
-  //otherObject.setLinearVelocity({x: 2000, y:200, z: 200});//new Vector3(0,200,200));
- otherObject.applyCentralImpulse({x: 200, y:200, z: 200});
-}
-
-box.on('collision', fnCollide);
-
-const box1 = new WHS.Box({
   geometry: {
     width: 20,
     height: 20,
     depth: 20
   },
   
-  mass: 1, // Mass of physics object.
-
-  material: {
-    color: 0xF2aaaa,
-    kind: 'lambert'
-  },
-
-  position: {
-    x: 0,
-    y: 50,
-    z: 0
-  }
-   
+  modules: [
+    new PHYSICS.BoxModule({
+      mass: 10,
+      //friction: 0
+    })
+  ],
+  
+  material: new THREE.MeshBasicMaterial({color: 0xff0000})
 });
 
-box.addTo(world);
-box1.addTo(world);
+// Conveyor
+const conveyor = new WHS.Box({
+  geometry: {
+    width: 100,
+    height: 2,
+    depth: 1000
+  },
+  
+  modules: [
+    new PHYSICS.BoxModule({
+      mass: 1000,
+      friction: 0.8
+    })
+  ],
+  
+  material: new THREE.MeshBasicMaterial({color: 0xaaaaaa})
+});
+conveyor.on('collision', (otherObject, v, r, contactNormal) => {
+  //window.alert("Contact");
+  otherObject.component.applyCenterImpulse(new THREE.Vector3(0, 0, 160));//new Vector3(2000,0,0));//{x: 20000, y:0, z: 0});
+});
+conveyor.addTo(app); 
+box.addTo(app);
+// Lights
+new WHS.PointLight({
+  light: {
+    intensity: 0.5,
+    distance: 100
+  },
 
-//box1.applyCentralImpulse(0); // unusual behaviour box1 disappears
-//box1.applyCentralImpulse({x: 2000, y:200, z: 200}); // <- this works
-//applyCentralImpulse
-//sphere.addTo(world);
-plane.addTo(world);
+  shadow: {
+    fov: 90
+  },
 
-window.alert("Starting...");
+  position: new THREE.Vector3(0, 10, 10)
+}).addTo(app);
 
-world.start(); // Start animations and physics simulation.
-world.setControls(new WHS.OrbitControls());
+new WHS.AmbientLight({
+  light: {
+    intensity: 0.4
+  }
+}).addTo(app);
+
+app.start();
